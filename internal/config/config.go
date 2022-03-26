54260722201bc2e6fa6b60855abd81156b706f9e
Ben Ferreira <benferreira@users.noreply.github.com>
2022-03-25 23:58:16 -0600

    Revised configuration (#3)
    feat!: revising configuration to use environment variables
    
    BREAKING CHANGE: flags are no longer supported

---
+++ internal/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Debug     bool
+	Port      int
+	PrettyLog bool
+	RsaPubKey string
+	TestMode  bool
+}
+
+func NewConfig() (*Config, error) {
+	conf := Config{
+		Debug:    false,
+		Port:     8000,
+		TestMode: false,
+	}
+
+	if _, ok := os.LookupEnv("DEBUG"); ok {
+		conf.Debug = true
+	}
+
+	if port, ok := os.LookupEnv("PORT"); ok {
+		portInt, err := strconv.Atoi(port)
+
+		if err != nil {
+			return nil, fmt.Errorf("invalid value for PORT: error: %v", err)
+		}
+
+		conf.Port = portInt
+	}
+
+	if _, ok := os.LookupEnv("PRETTY_LOGGING"); ok {
+		conf.PrettyLog = true
+	}
+
+	if _, ok := os.LookupEnv("TEST_MODE"); ok {
+		conf.TestMode = true
+		return &conf, nil
+	}
+
+	if rsaKey, ok := os.LookupEnv("RSA_PUB_KEY"); ok {
+		conf.RsaPubKey = rsaKey
+	} else {
+		return nil, fmt.Errorf("RSA_PUB_KEY env var must be provided")
+	}
+
+	return &conf, nil
+}
