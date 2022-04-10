87cd4fbd631ad54520784b94437be0004f8d53d6
Ben Ferreira <ben.ferreira@gmail.com>
2022-04-10 07:59:46 -0600

    test(jwks): jwt sig validation with jwks

--- internal/jwks/jwks_test.go
+++ internal/jwks/jwks_test.go
@@ -5,12 +5,15 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
 	"jwks-server/internal/config"
 	"jwks-server/internal/jwks"
 	"jwks-server/internal/rsa_helper"
 	"testing"
 
+	"github.com/MicahParks/keyfunc"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -70,6 +73,38 @@ func TestNewJWK(t *testing.T) {
 	jwk, err := jwks.NewJWK(config.RSAPubKey{Key: pubKeyString})
 	assert.Nil(t, err, "should not have errored")
 	assert.NotNil(t, jwk, "should have generated jwk")
+}
+
+// TestJWKSisValid generates a new JWT we sign ourselves with a new RSA key. We then verify that our JWKS generated
+// from the RSA key can be used to successfully validate the JWT.
+func TestJWKSisValid(t *testing.T) {
+	//Generate JWT and sign it with new private key
+	token := jwt.New(jwt.SigningMethodRS256)
+	token.Header["kid"] = "kid1"
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	tokenString, err := token.SignedString(privateKey)
+	assert.Nil(t, err)
+	assert.NotNil(t, tokenString)
+
+	// populate a JWKS with public key
+	jwksJson := generateJWKSJson(privateKey, "kid1")
+
+	// Take our jwks and provide it to the JWT parser
+	jwksKf, err := keyfunc.NewJSON(jwksJson)
+	assert.Nil(t, err)
+
+	parsedToken, err := jwt.Parse(tokenString, jwksKf.Keyfunc)
+	assert.Nil(t, err, "token failed to parse")
+	assert.True(t, parsedToken.Valid, "token should be valid")
+}
+
+func generateJWKSJson(privateKey *rsa.PrivateKey, kid string) []byte {
+	publicKeyString, _ := marshalToPem(privateKey)
+	pubKey := config.RSAPubKey{Key: publicKeyString, Kid: kid}
+	jwks, _ := jwks.NewJWKS(&config.RSAPubKeys{RSAPubKeys: []config.RSAPubKey{pubKey}})
+	jwksJson, _ := json.Marshal(jwks)
+
+	return jwksJson
 }
 
 func marshalToPem(privateKey *rsa.PrivateKey) (string, error) {
